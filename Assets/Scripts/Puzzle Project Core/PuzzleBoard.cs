be4c73adb685db3204450c429391719f8c1d03cd
Colin Whiteford <colinwhiteford516@gmail.com>
2026-03-31 00:51:49 -0400

    Puzzle Board Layout Fixed

--- Assets/Scripts/Puzzle Project Core/PuzzleBoard.cs
+++ Assets/Scripts/Puzzle Project Core/PuzzleBoard.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,34 +8,41 @@ public class PuzzleBoard : MonoBehaviour
     [SerializeField] uint m_shuffleMoveTotal;
     Grid2D<GridTile<PuzzlePiece>> m_grid;
     Vector2Int m_emptyCell;
-    void Start()
+    IEnumerator Start()
     {
-        m_grid = new((uint)m_solution.Grid.Rows, (uint)m_solution.Grid.Columns, 1f, Vector2.zero);
-        for (int w = 0; w < m_grid.Width; w++)
+        m_grid = new((uint)m_solution.Grid.Columns, (uint)m_solution.Grid.Rows, 1f, Vector2.zero);
+        for (int x = 0; x < m_grid.Width; x++)
         {
-            for (int h = 0; h < m_grid.Height; h++)
+            for (int y = 0; y < m_grid.Height; y++)
             {
-                if (m_solution.Grid[w, h] == null)
+                int row = (m_solution.Grid.Rows - 1) - y;
+                if (m_solution.Grid[row, x] == null)
                 {
-                    m_emptyCell = new(w, h);
-                    m_grid[w, h] = null;
+                    m_emptyCell = new(x, y);
+                    m_grid[x, y] = null;
                     continue;
                 }
-                m_grid[w, h] = new(Instantiate(m_solution.Grid[w, h]), m_grid, w, h);
+                m_grid[x, y] = new(Instantiate(m_solution.Grid[row, x]), m_grid, x, y);
+                m_grid[x, y].Value.transform.position = m_grid.GetCellCenter(new(x, y));
             }
         }
+        Vector2Int? lastNeighborPos = null;
         for (int i = 0; i < m_shuffleMoveTotal; i++)
         {
             List<GridTile<PuzzlePiece>> neighbors = GetCellNeighbors(m_emptyCell.x, m_emptyCell.y);
-            if (neighbors.Count <= 0) break; 
+            if (lastNeighborPos.HasValue)
+                neighbors.RemoveAll(n => n.Position == lastNeighborPos.Value);
+            if (neighbors.Count <= 0) break;
             GridTile<PuzzlePiece> selectedNeighbor = neighbors[Random.Range(0, neighbors.Count)];
             Vector2Int neighborOldPos = selectedNeighbor.Position;
             m_grid[m_emptyCell] = selectedNeighbor;
             m_grid[neighborOldPos] = null;
             selectedNeighbor.Position = m_emptyCell;
-            selectedNeighbor.Value.transform.position = m_grid.GetCellCenter(m_emptyCell);
+            yield return MoveTo(selectedNeighbor, m_emptyCell, 0.1f);
+            lastNeighborPos = m_emptyCell;
             m_emptyCell = neighborOldPos;
         }
+        yield return null;
     }
     List<GridTile<PuzzlePiece>> GetCellNeighbors(int x, int y)
     {
@@ -51,5 +59,16 @@ public class PuzzleBoard : MonoBehaviour
             neighbors.Add(tile);
         }
         return neighbors;
+    }
+
+    IEnumerator MoveTo(GridTile<PuzzlePiece> pieceToMove, Vector2Int newPosition, float time)
+    {
+        float elapsed = 0f;
+        while (elapsed < time)
+        {
+            elapsed += Time.deltaTime;
+            pieceToMove.Value.transform.position = Vector3.Lerp(pieceToMove.Value.transform.position, m_grid.GetCellCenter(newPosition), elapsed / time);
+            yield return null;
+        }
     }
 }
